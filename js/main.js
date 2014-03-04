478d5bce0b3b4d2a8bc3e9fb6028ea449bd3b11e
vellow <seaplor@gmail.com>
2014-03-04 16:05:16 +0800

    localJsonRemoveAdd

--- js/main.js
+++ js/main.js
@@ -5,11 +5,35 @@
 
 var app = angular.module('myApp', ['ngGrid','ui.bootstrap']);
 
+app.service('sharedProperties',function(){
+  var allData =    [{"name": "王二", "age": 32, "birthday": "Aug 3, 1978", "salary": 6000},
+                    {"name": "李狗", "age": 25, "birthday": "Aug 3, 1988", "salary": 6000},
+                    { "name": "Moroni", "age": 50, "birthday": "Oct 28, 1970", "salary": 5000},
+                    { "name": "Moris", "age": 43, "birthday": "Oct 28, 1970", "salary": 5000},
+                    { "name": "Nicolas", "age": 42, "birthday": "Oct 28, 1970", "salary": 5000},
+                    { "name": "Tiancum", "age": 43, "birthday": "Feb 12, 1985", "salary": 70000},
+                    { "name": "Jacob", "age": 27, "birthday": "Aug 23, 1983", "salary": 40000},
+                    { "name": "Nephi", "age": 29, "birthday": "May 31, 2010", "salary": 50000 },
+                    { "name": "Enos", "age": 34, "birthday": "Aug 3, 2008", "salary": 30000 }]; 
+  return {
+    getAllData : function(){
+      return allData;
+    },
+    setAllData : function(val){
+      allData = val;
+    }
+  }
+})
 
-app.controller("MyCtrl4",function($scope, $timeout, $http){
+
+app.controller("MyCtrl4",function($scope, $timeout, sharedProperties){
  
   $scope.mySelections = [];
   $scope.myData = [];
+  $scope.watchAdd = sharedProperties.getAllData();
+
+  setInterval(function(){$scope.watchAdd = sharedProperties.getAllData()},500);
+
   $scope.total_salary = function(){
     var total_s = 0,data=$scope.myData;
     for(var i=0;i<data.length;i++){
@@ -17,19 +41,14 @@ app.controller("MyCtrl4",function($scope, $timeout, $http){
     }
     return total_s
   };
-  
+
   $scope.deleteSelected = function() {
     angular.forEach($scope.mySelections, function(rowItem) { 
-        $scope.myData.splice($scope.myData.indexOf(rowItem),1);
+        $scope.watchAdd.splice($scope.watchAdd.indexOf(rowItem),1);
       });
+    // sharedProperties.setAllData($scope.myData);
     $scope.addAlert({type: 'danger', msg: "成功删除"+$scope.mySelections.length+"项"});
-    // $scope.mySelections = [];
     $scope.gridOptions.selectAll(false);
-  };
-
-  $scope.addPerson = function(){
-    $scope.myData.unshift({name: $scope.name, age: $scope.age, birthday: $scope.birthday, salary: $scope.salary});
-    $scope.addAlert({type: 'success', msg: "成功添加职员："+$scope.name})
   };
 
   $scope.clearInput = function(){
@@ -39,7 +58,7 @@ app.controller("MyCtrl4",function($scope, $timeout, $http){
   };
 
   $scope.save = function(){
-    $http.post('jsonFiles/register.json', $scope.myData);
+    // $http.post('jsonFiles/register.json', $scope.myData);
     $scope.addAlert({type: 'success', msg: "保存成功"})
   };
 
@@ -75,31 +94,35 @@ app.controller("MyCtrl4",function($scope, $timeout, $http){
     };
     $scope.getPagedDataAsync = function (pageSize, page, searchText) {
         setTimeout(function () {
-            var data;
+            var data = sharedProperties.getAllData();
             if (searchText) {
                 var ft = searchText.toLowerCase();
-                $http.get('jsonFiles/staffList.json').success(function (staffList) {    
-                    data = staffList.filter(function(item) {
+                data = data.filter(function(item) {
                         return JSON.stringify(item).toLowerCase().indexOf(ft) != -1;
                     });
-                    $scope.setPagingData(data,page,pageSize);
-                });            
+                $scope.setPagingData(data,page,pageSize);
             } else {
-                $http.get('jsonFiles/staffList.json').success(function (staffList) {
-                    $scope.setPagingData(staffList,page,pageSize);
-                });
+                $scope.setPagingData(data,page,pageSize);
             }
         }, 100);
     };
-  
+    
+
+
     $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
   
     $scope.$watch('pagingOptions', function (newVal, oldVal) {
-        if (newVal !== oldVal && newVal.currentPage !== oldVal.currentPage) {
+        if (newVal !== oldVal) {
           $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
         }
     }, true);
     $scope.$watch('filterOptions', function (newVal, oldVal) {
+        if (newVal !== oldVal) {
+          $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+        }
+    }, true);
+
+    $scope.$watch('watchAdd', function (newVal, oldVal) {
         if (newVal !== oldVal) {
           $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
         }
@@ -120,55 +143,10 @@ app.controller("MyCtrl4",function($scope, $timeout, $http){
                       { field: "age", cellFilter: 'number'},
                       { field: "birthday", cellFilter: 'date'},
                       { field: "salary", cellFilter: 'currency'}],
-      totalServerItems: 'totalServerItems',
+      totalServerItems: $scope.testGrid,
       pagingOptions: $scope.pagingOptions,
       filterOptions: $scope.filterOptions
     };
-/*
-    $scope.Val = "topval";
-    $scope.ModalCtrl = function($scope,$modal,$log){
-
-      $scope.items = ['item1', 'item2', 'item3'];
-
-      $scope.open = function () {
-
-        var modalInstance = $modal.open({
-          templateUrl: 'myModalContent.html',
-          controller: $scope.ModalInstanceCtrl,
-          resolve: {
-            items: function () {
-              return $scope.items;
-            }
-          }
-        });
-
-        modalInstance.result.then(function (selectedItem) {
-          $scope.selected = selectedItem;
-        }, function () {
-          $log.info('Modal dismissed at: ' + new Date());
-        });
-      };
-
-    };
-    $scope.ModalInstanceCtrl = function($scope, $modalInstance, items){
-        $scope.items = items;
-        $scope.selected = {
-          item: $scope.items[0]
-        };
-
-        $scope.ok = function () {
-          allData.unshift({name: $scope.name, age: $scope.age, birthday: $scope.birthday, salary: $scope.salary});
-          // allData.addAlert({type: 'success', msg: "成功添加职员："+$scope.name})
-          $modalInstance.close($scope.selected.item);
-          console.log($scope.name);
-        };
-
-        $scope.cancel = function () {
-          $modalInstance.dismiss('cancel');
-        };
-    }*/
-
-
 });
 
 app.controller('ModalCtrl', function($scope,$modal,$log){
@@ -196,15 +174,17 @@ app.controller('ModalCtrl', function($scope,$modal,$log){
 
 });
 
-app.controller('ModalInstanceCtrl',function($scope, $modalInstance, items){
+app.controller('ModalInstanceCtrl',function($scope, $modalInstance, items, sharedProperties){
   $scope.items = items;
     $scope.selected = {
       item: $scope.items[0]
     };
 
     $scope.ok = function () {
-      $parent.myData.unshift({name: $scope.name, age: $scope.age, birthday: $scope.birthday, salary: $scope.salary});
-      $parent.addAlert({type: 'success', msg: "成功添加职员："+$scope.name})
+      var data = sharedProperties.getAllData();
+      data.unshift({name: $scope.name, age: $scope.age, birthday: $scope.birthday, salary: $scope.salary});
+      sharedProperties.setAllData(data);
+      // $parent.addAlert({type: 'success', msg: "成功添加职员："+$scope.name})
       $modalInstance.close($scope.selected.item);
     };
 
